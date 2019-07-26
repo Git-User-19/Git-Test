d1e17f52e1461433ec2224edcf0e8d55caadfffb
wisdomlogix <nimisha@wisdomlogix.com>
2019-07-26 18:34:48 +0530

    changes of 123 and aaa

--- file1.php
+++ file1.php
@@ -1,13 +1,11 @@
 <?php
 /*
  * Filename: file1.php
- Cron using influencer_master table as below:
--Cron that run everyday once
--Queries the database for all influencers that are suspended (Get all records whose status is 3 [suspended] and suspendflag = W/M [Week/Month])
--Update "status=1(Active)" in influencer_master table to check whether suspension time finished.
+ this is test file.
 */
 
 echo "This is just for testing";
 
+echo "123456";
 //===========================
 ?>
