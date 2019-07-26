3603d0806c43d5153648f685a965b3bb439f3f47
Git-User-19 <53249788+Git-User-19@users.noreply.github.com>
2019-07-26 18:25:15 +0530

    Update file1.php

--- file1.php
+++ file1.php
@@ -1,10 +1,7 @@
 <?php
 /*
  * Filename: file1.php
- Cron using influencer_master table as below:
--Cron that run everyday once
--Queries the database for all influencers that are suspended (Get all records whose status is 3 [suspended] and suspendflag = W/M [Week/Month])
--Update "status=1(Active)" in influencer_master table to check whether suspension time finished.
+ This is testing
 */
 
 echo "This is just for testing";
